5ad0a3ccb8582cd462693e7e78a81537791795fe
Alexis Alulema <alexis.alulema@gmail.com>
2019-04-22 08:41:11 -0500

    Small changes to clean desktop

--- clear.sh
+++ clear.sh
@@ -1,3 +1,7 @@
 docker stop $(docker ps -qa)
 docker rm $(docker ps -qa)
 docker network rm $(docker network ls -q)
+docker rmi alulema/mongodb-config
+docker rmi alulema/mongodb
+rm countries.json*
+rm mongodb-keyfile
